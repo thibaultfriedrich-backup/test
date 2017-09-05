9a16ad82a99567471bae0301b229cea2c6910192
Thibault Friedrich <tf@elqui.fr>
2017-09-05 15:56:15 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+// word1 word2 https://github.com/thibaultfriedrich-backup/test-raven.git
+function toto () {
+
+}
+
+// https://github.com/thibaultfriedrich-backup/test-raven.git word3 word4
+function titi () {
+
+}
+
+/**
+ * word1 word2
+ * https://github.com/thibaultfriedrich-backup/test-raven.git
+ */
+function tutu () {
+
+}
+
+
+/*
+word1 word2
+https://github.com/thibaultfriedrich-backup/test-raven.git
+*/
+function tata () {
+
+}
